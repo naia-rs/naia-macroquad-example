6558254932ebd01d23d548320a6e436680739aaa
Connor Carpenter <naia.rust.crate@gmail.com>
2020-11-29 15:40:42 -0700

    cleanup

--- client/src/app.rs
+++ client/src/app.rs
@@ -1,11 +1,11 @@
 
-use log::info;
-
 use std::{net::SocketAddr, time::Duration};
 
 use naia_client::{ClientConfig, ClientEvent, NaiaClient};
 
-use naia_qs_example_shared::{get_shared_config, manifest_load, AuthEvent, ExampleActor, ExampleEvent, KeyCommand, shared_behavior, PointActorColor};
+use naia_mq_example_shared::{get_shared_config, manifest_load, AuthEvent, ExampleActor, ExampleEvent, KeyCommand, shared_behavior, PointActorColor};
+
+use miniquad::*;
 
 const SERVER_PORT: u16 = 14191;
 
@@ -17,23 +17,17 @@ cfg_if! {
     }
 }
 
-extern crate quicksilver;
-
-use quicksilver::{geom::{Rectangle, Vector}, graphics::{Color, Graphics}, input::{Input, Key}, Result, Settings, Window, Timer};
-
-pub fn get_settings() -> Settings {
-    let mut settings = Settings::default();
-    settings.size = Vector::new(1280.0, 720.0);
-    settings
+pub struct App {
+    client: NaiaClient<ExampleEvent, ExampleActor>,
+    pawn_key: Option<u16>,
+    queued_command: Option<KeyCommand>,
 }
 
-pub async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
+impl App {
+    pub fn new() -> Self {
+        info!("Naia Miniquad Client Example Started");
 
-    // Naia
-
-    info!("Naia Miniquad Client Example Started");
-
-    cfg_if! {
+        cfg_if! {
             if #[cfg(target_arch = "wasm32")] {
                 // Put your Server's IP Address here!, can't easily find this automatically from the browser
                 let server_ip_address: IpAddr = "192.168.86.38".parse().expect("couldn't parse input IP address");
@@ -42,146 +36,165 @@ pub async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<
             }
         }
 
-    let server_socket_address = SocketAddr::new(server_ip_address, SERVER_PORT);
+        let server_socket_address = SocketAddr::new(server_ip_address, SERVER_PORT);
 
-    let mut client_config = ClientConfig::default();
-    client_config.heartbeat_interval = Duration::from_secs(2);
-    client_config.disconnection_timeout_duration = Duration::from_secs(5);
+        let mut client_config = ClientConfig::default();
+        client_config.heartbeat_interval = Duration::from_secs(2);
+        client_config.disconnection_timeout_duration = Duration::from_secs(5);
 
-    let auth = ExampleEvent::AuthEvent(AuthEvent::new("charlie", "12345"));
+        let auth = ExampleEvent::AuthEvent(AuthEvent::new("charlie", "12345"));
 
-    let mut client = NaiaClient::new(
-        server_socket_address,
-        manifest_load(),
-        Some(client_config),
-        get_shared_config(),
-        Some(auth),
-    );
+        let mut client = NaiaClient::new(
+            server_socket_address,
+            manifest_load(),
+            Some(client_config),
+            get_shared_config(),
+            Some(auth),
+        );
 
-    // Quicksilver
+        let mut pawn_key: Option<u16> = None;
+        let mut queued_command: Option<KeyCommand> = None;
 
-    let square_size = Vector::new(32.0, 32.0);
+        App {
+            client,
+            pawn_key,
+            queued_command,
+        }
+    }
 
-    let mut frame_timer = Timer::time_per_second(60.0);
-
-    let mut pawn_key: Option<u16> = None;
-    let mut queued_command: Option<KeyCommand> = None;
-
-    loop {
-        while let Some(_) = input.next_event().await {}
-
-        if frame_timer.exhaust().is_some() {
-
-            // input
-            let w = input.key_down(Key::W);
-            let s = input.key_down(Key::S);
-            let a = input.key_down(Key::A);
-            let d = input.key_down(Key::D);
-
-            if let Some(command) = &mut queued_command {
-                if w { command.w.set(true); }
-                if s { command.s.set(true); }
-                if a { command.a.set(true); }
-                if d { command.d.set(true); }
-            } else {
-                queued_command = Some(KeyCommand::new(w, s, a, d));
-            }
-
-            // update
-            loop {
-                if let Some(result) = client.receive() {
-                    match result {
-                        Ok(event) => {
-                            match event {
-                                ClientEvent::Connection => {
-                                    info!("Client connected to: {}", client.server_address());
+    pub fn update(&mut self) {
+        if let Some(result) = self.client.receive() {
+            match result {
+                Ok(event) => {
+                    match event {
+                        ClientEvent::Connection => {
+                            info!("Client connected to: {}", self.client.server_address());
+                        }
+                        ClientEvent::Disconnection => {
+                            info!("Client disconnected from: {}", self.client.server_address());
+                        }
+                        ClientEvent::Tick => {
+                            if let Some(pawn_key) = self.pawn_key {
+                                if let Some(command) = self.queued_command.take() {
+                                    self.client.send_command(pawn_key, &command);
                                 }
-                                ClientEvent::Disconnection => {
-                                    info!("Client disconnected from: {}", client.server_address());
-                                }
-                                ClientEvent::Tick => {
-                                    if let Some(pawn_key) = pawn_key {
-                                        if let Some(command) = queued_command.take() {
-                                            client.send_command(pawn_key, &command);
-                                        }
-                                    }
-                                }
-                                ClientEvent::AssignPawn(local_key) => {
-                                    pawn_key = Some(local_key);
-                                    info!("assign pawn");
-                                }
-                                ClientEvent::UnassignPawn(_) => {
-                                    pawn_key = None;
-                                    info!("unassign pawn");
-                                }
-                                ClientEvent::Command(pawn_key, command_type) => {
-                                    match command_type {
-                                        ExampleEvent::KeyCommand(key_command) => {
-                                            if let Some(typed_actor) = client.get_pawn_mut(&pawn_key) {
-                                                match typed_actor {
-                                                    ExampleActor::PointActor(actor) => {
-                                                        shared_behavior::process_command(&key_command, actor);
-                                                    }
-                                                }
+                            }
+                        }
+                        ClientEvent::AssignPawn(local_key) => {
+                            self.pawn_key = Some(local_key);
+                            info!("assign pawn");
+                        }
+                        ClientEvent::UnassignPawn(_) => {
+                            self.pawn_key = None;
+                            info!("unassign pawn");
+                        }
+                        ClientEvent::Command(pawn_key, command_type) => {
+                            match command_type {
+                                ExampleEvent::KeyCommand(key_command) => {
+                                    if let Some(typed_actor) = self.client.get_pawn_mut(&pawn_key) {
+                                        match typed_actor {
+                                            ExampleActor::PointActor(actor) => {
+                                                shared_behavior::process_command(&key_command, actor);
                                             }
                                         }
-                                        _ => {}
                                     }
                                 }
                                 _ => {}
                             }
                         }
-                        Err(err) => {
-                            info!("Client Error: {}", err);
-                        }
+                        _ => {}
                     }
-                } else {
-                    break;
+                }
+                Err(err) => {
+                    info!("Client Error: {}", err);
                 }
             }
-
-            // drawing
-            gfx.clear(Color::BLACK);
-
-            if client.has_connection() {
-                // draw actors
-                for actor_key in client.actor_keys().unwrap() {
-                    if let Some(actor) = client.get_actor(&actor_key) {
-                        match actor {
-                            ExampleActor::PointActor(point_actor) => {
-                                let rect = Rectangle::new(
-                                    Vector::new(
-                                        f32::from(*(point_actor.as_ref().borrow().x.get())),
-                                        f32::from(*(point_actor.as_ref().borrow().y.get()))),
-                                    square_size);
-                                match point_actor.as_ref().borrow().color.get() {
-                                    PointActorColor::Red => gfx.fill_rect(&rect, Color::RED),
-                                    PointActorColor::Blue => gfx.fill_rect(&rect, Color::BLUE),
-                                    PointActorColor::Yellow => gfx.fill_rect(&rect, Color::YELLOW),
-                                }
-                            }
-                        }
-                    }
-                }
-
-                // draw pawns
-                for pawn_key in client.pawn_keys().unwrap() {
-                    if let Some(actor) = client.get_pawn(&pawn_key) {
-                        match actor {
-                            ExampleActor::PointActor(point_actor) => {
-                                let rect = Rectangle::new(
-                                    Vector::new(
-                                        f32::from(*(point_actor.as_ref().borrow().x.get())),
-                                        f32::from(*(point_actor.as_ref().borrow().y.get()))),
-                                    square_size);
-                                gfx.fill_rect(&rect, Color::WHITE);
-                            }
-                        }
-                    }
-                }
-            }
-
-            gfx.present(&window)?;
         }
     }
+
+    pub fn draw(&mut self, mut ctx: &Context) {
+        ctx.clear(Some((0., 0., 0., 0.)), None, None);
+
+//        if client.has_connection() {
+//            // draw actors
+//            for actor_key in client.actor_keys().unwrap() {
+//                if let Some(actor) = client.get_actor(&actor_key) {
+//                    match actor {
+//                        ExampleActor::PointActor(point_actor) => {
+//                            let rect = Rectangle::new(
+//                                Vector::new(
+//                                    f32::from(*(point_actor.as_ref().borrow().x.get())),
+//                                    f32::from(*(point_actor.as_ref().borrow().y.get()))),
+//                                square_size);
+//                            match point_actor.as_ref().borrow().color.get() {
+//                                PointActorColor::Red => gfx.fill_rect(&rect, Color::RED),
+//                                PointActorColor::Blue => gfx.fill_rect(&rect, Color::BLUE),
+//                                PointActorColor::Yellow => gfx.fill_rect(&rect, Color::YELLOW),
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//            // draw pawns
+//            for pawn_key in client.pawn_keys().unwrap() {
+//                if let Some(actor) = client.get_pawn(&pawn_key) {
+//                    match actor {
+//                        ExampleActor::PointActor(point_actor) => {
+//                            let rect = Rectangle::new(
+//                                Vector::new(
+//                                    f32::from(*(point_actor.as_ref().borrow().x.get())),
+//                                    f32::from(*(point_actor.as_ref().borrow().y.get()))),
+//                                square_size);
+//                            gfx.fill_rect(&rect, Color::WHITE);
+//                        }
+//                    }
+//                }
+//            }
+//        }
+    }
 }
+
+//pub async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
+//
+//    // Quicksilver
+//
+//    let square_size = Vector::new(32.0, 32.0);
+//
+//    let mut frame_timer = Timer::time_per_second(60.0);
+//
+//    let mut pawn_key: Option<u16> = None;
+//    let mut queued_command: Option<KeyCommand> = None;
+//
+//    loop {
+//        while let Some(_) = input.next_event().await {}
+//
+//        if frame_timer.exhaust().is_some() {
+//
+//            // input
+//            let w = input.key_down(Key::W);
+//            let s = input.key_down(Key::S);
+//            let a = input.key_down(Key::A);
+//            let d = input.key_down(Key::D);
+//
+//            if let Some(command) = &mut queued_command {
+//                if w { command.w.set(true); }
+//                if s { command.s.set(true); }
+//                if a { command.a.set(true); }
+//                if d { command.d.set(true); }
+//            } else {
+//                queued_command = Some(KeyCommand::new(w, s, a, d));
+//            }
+//
+//            // update
+//            loop {
+//
+//            }
+//
+//            // drawing
+//            gfx.clear(Color::BLACK);
+//
+//
+//        }
+//    }
+//}
--- client/src/main.rs
+++ client/src/main.rs
@@ -3,27 +3,131 @@ extern crate cfg_if;
 
 use miniquad::*;
 
-mod app;
+#[repr(C)]
+struct Vec2 {
+    x: f32,
+    y: f32,
+}
+#[repr(C)]
+struct Vertex {
+    pos: Vec2,
+    uv: Vec2,
+}
 
+mod app;
 use app::App;
 
 struct Stage {
     ctx: Context,
     app: App,
+    pipeline: Pipeline,
+    bindings: Bindings,
 }
+
+impl Stage {
+    pub fn new(mut ctx: Context, app: App) -> Self {
+        #[rustfmt::skip]
+        let vertices: [Vertex; 4] = [
+            Vertex { pos : Vec2 { x: -0.5, y: -0.5 }, uv: Vec2 { x: 0., y: 0. } },
+            Vertex { pos : Vec2 { x:  0.5, y: -0.5 }, uv: Vec2 { x: 1., y: 0. } },
+            Vertex { pos : Vec2 { x:  0.5, y:  0.5 }, uv: Vec2 { x: 1., y: 1. } },
+            Vertex { pos : Vec2 { x: -0.5, y:  0.5 }, uv: Vec2 { x: 0., y: 1. } },
+        ];
+        let vertex_buffer = Buffer::immutable(&mut ctx, BufferType::VertexBuffer, &vertices);
+
+        let indices: [u16; 6] = [0, 1, 2, 0, 2, 3];
+        let index_buffer = Buffer::immutable(&mut ctx, BufferType::IndexBuffer, &indices);
+
+        let pixels: [u8; 4] = [
+            0xFF, 0xFF, 0xFF, 0xFF,
+        ];
+        let texture = Texture::from_rgba8(&mut ctx, 1, 1, &pixels);
+
+        let bindings = Bindings {
+            vertex_buffers: vec![vertex_buffer],
+            index_buffer,
+            images: vec![texture],
+        };
+
+        let shader = Shader::new(&mut ctx, shader::VERTEX, shader::FRAGMENT, shader::meta()).unwrap();
+
+        let pipeline = Pipeline::new(
+            &mut ctx,
+            &[BufferLayout::default()],
+            &[
+                VertexAttribute::new("pos", VertexFormat::Float2),
+                VertexAttribute::new("uv", VertexFormat::Float2),
+            ],
+            shader,
+        );
+
+        Stage { pipeline, bindings, app, ctx }
+    }
+}
+
 impl EventHandlerFree for Stage {
     fn update(&mut self) {
         self.app.update();
     }
 
     fn draw(&mut self) {
-        self.ctx.clear(Some((0., 1., 0., 1.)), None, None);
+        let t = date::now();
+
+        self.ctx.begin_default_pass(Default::default());
+
+        self.ctx.apply_pipeline(&self.pipeline);
+        self.ctx.apply_bindings(&self.bindings);
+
+        self.ctx.apply_uniforms(&shader::Uniforms {
+            offset: (0.0, 0.0),
+        });
+        self.ctx.draw(0, 6, 1);
+        self.ctx.end_render_pass();
+
+        self.ctx.commit_frame();
+
+//        self.app.draw(&self.ctx);
     }
 }
 
 fn main() {
     let app = App::new();
     miniquad::start(conf::Conf::default(), |ctx| {
-        UserData::free(Stage { ctx, app })
+        UserData::free(Stage::new(ctx, app))
     });
+}
+
+mod shader {
+    use miniquad::*;
+
+    pub const VERTEX: &str = r#"#version 100
+    attribute vec2 pos;
+    attribute vec2 uv;
+    uniform vec2 offset;
+    varying lowp vec2 texcoord;
+    void main() {
+        gl_Position = vec4(pos + offset, 0, 1);
+        texcoord = uv;
+    }"#;
+
+    pub const FRAGMENT: &str = r#"#version 100
+    varying lowp vec2 texcoord;
+    uniform sampler2D tex;
+    void main() {
+        gl_FragColor = texture2D(tex, texcoord);
+    }"#;
+
+    pub fn meta() -> ShaderMeta {
+        ShaderMeta {
+            images: vec!["tex".to_string()],
+            uniforms: UniformBlockLayout {
+                uniforms: vec![UniformDesc::new("offset", UniformType::Float2)],
+            },
+        }
+    }
+
+    #[repr(C)]
+    pub struct Uniforms {
+        pub offset: (f32, f32),
+    }
 }
